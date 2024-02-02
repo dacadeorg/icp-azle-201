7e80efb77532cc7ecb07d8cff352219fccc7e849
Elias Datler <46360620+fxgst@users.noreply.github.com>
2024-02-02 11:29:10 +0000

    update canister urls script

--- canister_urls.py
+++ canister_urls.py
@@ -9,7 +9,7 @@ BOLD='\033[1m'
 
 def get_host():
     if 'GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN' in environ:
-        return f"https://${environ['CODESPACE_NAME']}-4943.${environ['GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN']}"
+        return f"https://{environ['CODESPACE_NAME']}-4943.{environ['GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN']}"
     else:
         return 'http://127.0.0.1:4943'
 
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     root_path = path.dirname(path.realpath(__file__))
     canister_id_file = f'{root_path}/.dfx/local/canister_ids.json';
     if not path.exists(canister_id_file):
-        print(f"Run 'dfx deploy' first.")
+        print(f"Follow the readme to deploy the canisters first.")
         exit(1)
     with open(canister_id_file) as f:
         data = json.load(f)
@@ -34,11 +34,12 @@ if __name__ == "__main__":
 
         host = get_host()
         
-        print(f"""{BOLD}URLs:
-  Frontend canister via browser""")
-        for name, id in frontend_canisters:
-            print(f"    {name}: {GREEN}{host}/?canisterId={id}{NORMAL}{BOLD}")
-        print("  Backend canister via Candid interface:")
-        for name, id in backend_canisters:
-            print(f"    {name}: {GREEN}{host}/?canisterId={candid_ui_canister_id}&id={id}{NORMAL}{BOLD}")
-        print(f"{NORMAL}", end="")
+        print(f"{BOLD}URLs:{NORMAL}")
+        if frontend_canisters:
+            print(f"""{BOLD}  Frontend canister via browser{NORMAL}""")
+            for name, id in frontend_canisters:
+                print(f"{BOLD}    {name}: {GREEN}{host}/?canisterId={id}{NORMAL}")
+        if backend_canisters:
+            print(f"{BOLD}  Backend canister via Candid interface:")
+            for name, id in backend_canisters:
+                print(f"{BOLD}    {name}: {GREEN}{host}/?canisterId={candid_ui_canister_id}&id={id}{NORMAL}")
